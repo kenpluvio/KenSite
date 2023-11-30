431409a1be54fe1ebde7549565dc852da22ab7e3
kenpluvio <kenpluvio@gmail.com>
2023-11-29 20:13:00 -0500

    add javascript code, change the comment area

---
+++ script.js
@@ -0,0 +1,48 @@
+const endOfED = document.querySelector("#main-content");
+const connectBtn = document.querySelector("#ConnectMe");
+
+class textArea {
+    constructor(
+        name,
+        id,
+        rows,
+        cols,
+        hidden,
+        exist
+    ) {
+        this.name = name;
+        this.id = id;
+        this.rows = rows;
+        this.cols = cols;
+        this.hidden = hidden;
+        this.exist = exist;
+    }
+}
+
+const newCMArea = new textArea("textArea", "textArea", 4, 50, true, false);
+const commentArea = document.createElement("div");
+commentArea.setAttribute("id", "commentArea")
+commentArea.setAttribute("style", "display: none")
+commentArea.innerHTML= `
+    <label for=${newCMArea.name}>Enter Text:</label>
+    <textarea id=${newCMArea.id} name=${newCMArea.name} rows=${newCMArea.rows} cols=${newCMArea.cols}></textarea>
+`;
+
+connectBtn.addEventListener("click",()=>{
+    // let newCMAreaObject = document.querySelector(newCMArea.id)
+    if (newCMArea.exist === false) {
+        endOfED.append(commentArea);
+        newCMArea.exist = true;
+    }
+    if (newCMArea.hidden === true) {
+        commentArea.style.display = "block";
+        //move to the window of commentArea
+        commentArea.scrollIntoView({ behavior: "smooth" });
+        newCMArea.hidden = false;
+    } else {
+        commentArea.style.display = "none";
+        newCMArea.hidden = true;
+    }
+},false);
+
+
